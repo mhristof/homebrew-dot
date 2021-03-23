27dbaad3ea710f42194d69eb123ee4d4c2f192d0
Mike Christof <mhristof@gmail.com>
2021-03-23 13:50:08 +0000

    fix formula

--- Formula/Files.rb
+++ Formula/Files.rb
@@ -1,15 +1,17 @@
 class Files < Formula
   desc "Install mikes dotfiles"
   homepage ""
-  #url "https://example.com/foo-0.1.tar.gz"
-  #sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
-  #license ""
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  version "1.0"
+  #version ""
 
   # depends_on "cmake" => :build
 
   def install
     # ENV.deparallelize
-    system "touch /tmp/foo.formula"
+    system "touch", "#{prefix}/foo.formula"
+    #system "touch /tmp/foo.formula"
   end
 
   test do
