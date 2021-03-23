c3fc634cecfc6f02997833864f2b930579c08f05
Mike Christof <mhristof@gmail.com>
2021-03-23 12:03:26 +0000

    install first files

---
+++ Formula/Files.rb
@@ -0,0 +1,18 @@
+class Foo < Formula
+  desc "Install mikes dotfiles"
+  homepage ""
+  #url "https://example.com/foo-0.1.tar.gz"
+  #sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
+  #license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize
+    system "touch /tmp/foo.formula"
+  end
+
+  test do
+    system "true"
+  end
+end
